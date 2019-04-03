722466ae8601561b0dd882941399f1509c396993
Ryan <heatonr@mail.gvsu.edu>
2019-04-02 20:50:30 -0400

    Created project

---
+++ src/LinkedList/clipBdLinkedList.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class clipBdLinkedList {
+    private NodeCB top;
+    private NodeCB tail;
+
+    public clipBdLinkedList() {
+        tail = top = null;
+    }
+
+    // create methods you need.
+
+}
