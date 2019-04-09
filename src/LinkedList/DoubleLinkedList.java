3b29a3375be4bcecc82bd137de3ebae23d2681be
ElenaXiao <48529578+ElenaXiao@users.noreply.github.com>
2019-04-09 17:28:09 -0400

    Add files via upload

--- src/LinkedList/DoubleLinkedList.java
+++ src/LinkedList/DoubleLinkedList.java
@@ -1,55 +1,75 @@
 package LinkedList;
 
-public class DoubleLinkedList<E>  {
-	protected NodeD<E> top;      // The first NodeD<E> in the list
 
-    // This instance variable is not required, however if you
-    // find it useful, then you are welcome to use it
-	protected NodeD<E> cursor;  // The current NodeD<E> in the list
+    public class DoubleLinkedList<E>  {
+        protected NodeD<E> top;      // The first NodeD<E> in the list
 
-	public DoubleLinkedList() {
-		top = null;
-		cursor = null;
-	}
+        // This instance variable is not required, however if you
+        // find it useful, then you are welcome to use it
+        protected NodeD<E> cursor;  // The current NodeD<E> in the list
 
-	public E get(int position) {
-		cursor = top;
-		for (int i = 0; i < position; i++)
-			cursor = cursor.getNext();
-		return cursor.getData();
-	}
+        public DoubleLinkedList() {
+            top = null;
+            cursor = null;
+        }
 
-	public void add(E item, int position) {
-		NodeD<E> placeHolderNode;
+        public E get(int position) {
+            cursor = top;
+            for (int i = 0; i < position; i++)
+                cursor = cursor.getNext();
+            return cursor.getData();
+        }
 
-		cursor = top;
-		for (int i = 0; i < position - 1; i++)
-			cursor = cursor.getNext();
-		if(cursor == null){
-			cursor = new NodeD<E>(item, null, null);
-			top = cursor;
-		}
-		else if (cursor.next == null) {
-			cursor.next = new NodeD<E>(item, null, cursor);
-		} else {
-			placeHolderNode = cursor.getNext();
-			cursor.next = new NodeD<E>(item, placeHolderNode, cursor);
-			placeHolderNode.prev = cursor.next;
-		}
-	}
+        public void add(E item, int position) {
+            NodeD<E> placeHolderNode;
 
-	public String toString() {
-		String retVal = "";
-		NodeD<E> cur = top;
-		while (cur != null) {
-			retVal += cur.getData();
-			cur = cur.getNext();
-		}
-		return retVal;
-	}
+            cursor = top;
+            for (int i = 0; i < position - 1; i++)
+                cursor = cursor.getNext();
+            if(cursor == null){
+                cursor = new NodeD<E>(item, null, null);
+                top = cursor;
+            }
+            else if (cursor.next == null) {
+                cursor.next = new NodeD<E>(item, null, cursor);
+            } else {
+                placeHolderNode = cursor.getNext();
+                cursor.next = new NodeD<E>(item, placeHolderNode, cursor);
+                placeHolderNode.prev = cursor.next;
+            }
+        }
 
 
-	// Create the rest of the needed methods.
+        public void delete(int position){
+            NodeD<E> placeHolderNode;
+
+            cursor = top;
+            for(int i = 0; i < position - 1; i++){
+                cursor = cursor.getNext();
+            }
+            if (cursor == null || cursor.next == null)
+                return;
+
+            NodeD<E> next = cursor.next.next;
+
+            cursor.next = next;
+
+        }
 
 
-}
+        public String toString() {
+            String retVal = "";
+            NodeD<E> cur = top;
+            while (cur != null) {
+                retVal += cur.getData();
+                cur = cur.getNext();
+            }
+            return retVal;
+        }
+
+
+        // Create the rest of the needed methods.
+
+
+    }
+
--- src/LinkedList/NodeCB.java
+++ src/LinkedList/NodeCB.java
@@ -1,26 +1,28 @@
 package LinkedList;
 
-public class NodeCB {
-    private int clipBoardNumber;
-    private NodeD<Character> topOfClipBoard;
-    private NodeCB next;
 
-    public NodeCB() {
+    public class NodeCB {
+        private int clipBoardNumber;
+        private NodeD<Character> topOfClipBoard;
+        private NodeCB next;
+
+        public NodeCB() {
+        }
+
+        public int getClipBoardNumber() {
+            return clipBoardNumber;
+        }
+
+        public void setClipBoardNumber(int clipBoardNumber) {
+            this.clipBoardNumber = clipBoardNumber;
+        }
+
+        public NodeD getTopOfClipBoard() {
+            return topOfClipBoard;
+        }
+
+        public void setTopOfClipBoard(NodeD topOfClipBoard) {
+            this.topOfClipBoard = topOfClipBoard;
+        }
     }
 
-    public int getClipBoardNumber() {
-        return clipBoardNumber;
-    }
-
-    public void setClipBoardNumber(int clipBoardNumber) {
-        this.clipBoardNumber = clipBoardNumber;
-    }
-
-    public NodeD getTopOfClipBoard() {
-        return topOfClipBoard;
-    }
-
-    public void setTopOfClipBoard(NodeD topOfClipBoard) {
-        this.topOfClipBoard = topOfClipBoard;
-    }
-}
